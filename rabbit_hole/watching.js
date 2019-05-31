0d8e01463ecec74ccfd95e1725a15f335812a86a
Andrew Peterson <andrew@andpeterson.com>
2019-05-31 11:57:48 -0700

    hEiSoNe

---
+++ rabbit_hole/watching.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+var Nyarlathotep = document.getElementById("AiNsLeYiShE")
+Nyarlathotep.onclick = function(){
+Nyarlathotep.style = "position: fixed; width: 100%; height: 100%; -webkit-filter: invert(1); filter: invert(1); top: 0px; left: 0px;"
+setTimeout(function(){
+    Nyarlathotep.style = ""
+}, 200);
+}
+}
